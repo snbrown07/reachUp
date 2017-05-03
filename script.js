40e1289e5046a17eef38ac81ee32b790b86dc562
snbrown07 <sbrow5081@withs.org>
2017-05-03 21:40:42 +0000

    Made things work

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function makeNewDiv(blog_name, title_post, url_post) {
+    var blogName= $("<h4>").append(blog_name);
+    var titlePost= $("<a>").append(title_post);
+    titlePost.attr("href", url_post)
+    var newDiv= $("<div>").append(blogName).append(titlePost);
+    newDiv.addClass("col-3");
+    $("#results").append(newDiv);
+}
+function makeNewTitle(searchTerm){
+    $("#resultTitle").append("Tumblr Post About " + searchTerm);
+}
 // this function takes a string (searchTerm) and queries the Tumblr API to find
 // posts tagged with that string
 var queryTumblrApi = function(searchTerm) {
@@ -16,6 +27,14 @@ var queryTumblrApi = function(searchTerm) {
         console.log(response);
         // TO-DOs
         // 1: add a for loop that goes over each item in the array of posts in the response
+        for(var i= 0; i< response.response.length; i++){
+            console.log(response.response[i]);
+            console.log(response.response[i].blog_name);
+            console.log(response.response[i].summary);
+            console.log(response.response[i].post_url);
+            makeNewDiv(response.response[i].blog_name, response.response[i].summary, response.response[i].post_url);
+        }
+        makeNewTitle(searchTerm);
         // 2: inside the loop, console.log each post
         // 3: console.log the blog name, post title, and post url for each post
         // 4: insert blog name and post title in the HTML of the results page
@@ -29,6 +48,8 @@ $(document).ready(function() {
 
     // this function happens when you click any button with the class "marsh"
     $(".marsh").click(function(event) {
+        $("#results").empty();
+        $("#resultTitle").empty();
         // get the text from the button
         var buttonName = event.currentTarget.innerText;
         // call the queryTumblrApi function with the text from the button
